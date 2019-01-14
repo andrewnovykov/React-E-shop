94973dd2aabbe90c5800b97353eb7661f15d42e4
papapin <papapin777#gmail.com>
2019-01-13 16:10:26 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
+import { Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
+import "bootstrap/dist/css/bootstrap.min.css";    
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import Details from './components/Details';
+import Default from "./components/Default";
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <React.Fragment>
+        <Navbar />
+        <Switch>
+          
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route exact path="/" component={ProductList} />
+          <Route  component={Default} />
+
+        </Switch>
+
+      </React.Fragment>;
   }
 }
 
